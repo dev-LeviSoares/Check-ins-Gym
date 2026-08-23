80539556d4a2aee5e5e94608ab7c625a02bd7704
dev-LeviSoares <dev.levi.soares@gmail.com>
2026-08-23 12:09:24 -0300

    fix(workflow):  correcao na sintaxe do worflox e correcao nos testes e2e

--- src/utils/test/create-and-authenticate-use.ts
+++ src/utils/test/create-and-authenticate-use.ts
@@ -1,12 +1,23 @@
 import type { FastifyInstance } from "fastify";
 import request from "supertest";
+import { prisma } from "../../lib/prisma.js";
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
   await request(app.server).post("/users").send({
     name: "John Doe",
     email: "JohnDoe@example.com",
     password: "123456",
   });
+
+  if (isAdmin) {
+    await prisma.user.update({
+      where: { email: "JohnDoe@example.com" },
+      data: { role: "ADMIN" },
+    });
+  }
 
   const authResponse = await request(app.server).post("/sessions").send({
     email: "JohnDoe@example.com",
@@ -16,6 +27,6 @@ export async function createAndAuthenticateUser(app: FastifyInstance) {
   const { token } = authResponse.body;
 
   return {
-    token
-  }
+    token,
+  };
 }
